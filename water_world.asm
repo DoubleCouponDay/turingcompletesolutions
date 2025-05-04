c254f0051c29edafac14df61f8a60a9e9f93c197
Doublecouponday <mail@samueljenks.me>
2025-05-04 19:13:16 +1200

    wip: water world

--- water_world.asm
+++ water_world.asm
@@ -3,32 +3,39 @@
 #reg2 = start point column
 #reg3 = windback column
 #reg4 = calculated volume
+#reg5 = previous column
 #reg5 = summed volume
 
 #create a new index for the volume array
 ADD|IMMALL 16 0 reg1
 
 label Loop
-#read the current column height
-ADD|IMMB input 0 reg2
-#store the column height in ram, first array
-STORE reg0 reg1 0 
-#if greater than previous column, start scanning backwards for the next local maximum
-IFGREAT
+	#read the current column height
+	ADD|IMMB input 0 reg2
+	#store the column height in ram, first array
+	STORE reg0 reg2 0	
+	IFNEQ|IMMB reg2 0 DontSkipColumn #skip the first column	
+	ADD|IMMB reg2 0 reg5 #make current column the previous column
+	ADD|IMMB reg0 1 reg0 #increment current column index
+	IFEQ|IMMALL 0 0 Loop
+	label DontSkipColumn
+	IFLESSE reg2 reg5 DontWindBack #if greater than previous column, and current column is not the first column, start scanning backwards for the next local maximum
 
-label WindbackLoop
-	#volume = difference of start point minus current column
-	
-	#overwrite the volume in ram
+	label WindbackLoop
+		LOAD reg1 
+		 #volume = difference of start point minus current column		
+		#overwrite the volume in ram
+		#store the volume in indexed ram cells. second array
 
-	#store the volume in indexed ram cells. second array
+	label DontWindBack
 
-#else continue down
-IFNEQ|IMMB reg1 16 WindbackLoop
-#increment column loop index
-ADD|IMMB reg0 1 reg0
-#if column index within bounds, continue looping
-IFNEQ|IMMB reg0 16 Loop
+	#else continue down
+	IFNEQ|IMMB reg1 16 WindbackLoop
+	#increment column loop index
+	ADD|IMMB reg0 1 reg0
+	#if column index within bounds, continue looping
+	IFNEQ|IMMB reg0 16 Loop
+
 #reset windback index
 ADD|IMMB reg1 16 reg1
 #sum all the second array values
