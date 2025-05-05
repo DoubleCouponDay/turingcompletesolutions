dc763b157e6037846a4f606f9378bfa60f9fc3f3
Samuel Jenks <mail@samueljenks.me>
2025-05-06 05:53:31 +1200

    draft: double loop diff and sum

--- water_world.asm
+++ water_world.asm
@@ -2,47 +2,46 @@
 #reg1 = windback index (16 - 31)
 #reg2 = start point column
 #reg3 = windback column
-#reg4 = calculated volume
-#reg5 = previous column
-#reg5 = summed volume
-
-#create a new index for the volume array
-ADD|IMMALL 16 0 reg1
+#reg4 = calculation
+#reg5 = previous column (algorithm stage)
+#reg5 = summed volume (final stage)
+#STACK = save point column index
 
 label Loop
-	#read the current column height
-	ADD|IMMB input 0 reg2
-	#store the column height in ram, first array
-	STORE reg0 reg2 0	
-	IFNEQ|IMMB reg2 0 DontSkipColumn #skip the first column	
-	ADD|IMMB reg2 0 reg5 #make current column the previous column
-	ADD|IMMB reg0 1 reg0 #increment current column index
-	IFEQ|IMMALL 0 0 Loop
-	label DontSkipColumn
-	IFLESSE reg2 reg5 DontWindBack #if greater than previous column, and current column is not the first column, start scanning backwards for the next local maximum
+	ADD|IMMB input 0 reg2 #read the current column height
+	STORE reg0 reg2 0 #store the column height in ram, first array
+	PUSH reg0 0 0 #save point column index
 
-	label WindbackLoop
-		LOAD reg1 
-		 #volume = difference of start point minus current column		
-		#overwrite the volume in ram
-		#store the volume in indexed ram cells. second array
+	IFNEQ|IMMB reg2 0 DontSkipColumn #skip the first column	
+		POP 0 0 reg0 #remove save point
+		ADD|IMMB reg2 0 reg5 #make current column the previous column
+		ADD|IMMB reg0 1 reg0 #increment current column index
+		IFEQ|IMMALL 0 0 Loop
+
+	label DontSkipColumn
+	IFLESSE reg2 reg5 DontWindBack #only sum when a positive gradient is detected
+
+	label WindbackLoop #start scanning backwards for the next local maximum
+		SUB|IMMB reg0 1 reg0 #decrement 
+		ADD|IMMB reg0 16 reg1 #set the windback index to second array, current column
+		LOAD reg0 0 reg3 #load the previous column
+		SUB reg2 reg3 reg4 #volume = difference of start point minus current windback column		
+		IFLESSE|IMMB reg4 0 StopWinding #if its bigger than or equal to the starting column, stop winding back
+		STORE reg1 reg4 0 #store the volume in second array 
+		IFNEQ|IMMB reg1 16 WindbackLoop
+		label StopWinding
 
 	label DontWindBack
+	POP 0 0 reg0 #remove save point
+	ADD|IMMB reg0 1 reg0 #increment column loop index
+	IFNEQ|IMMB reg0 16 Loop #if column index within bounds, continue looping
 
-	#else continue down
-	IFNEQ|IMMB reg1 16 WindbackLoop
-	#increment column loop index
-	ADD|IMMB reg0 1 reg0
-	#if column index within bounds, continue looping
-	IFNEQ|IMMB reg0 16 Loop
+ADD|IMMB reg1 16 reg1 #reset second array index
 
-#reset windback index
-ADD|IMMB reg1 16 reg1
-#sum all the second array values
 label SumLoop
-#load calculated volume
-LOAD reg1 0 reg4
-#add to the sum
-ADD reg4 reg5 reg5
-#send the calculation to output
-ADD|IMMB reg5 0 output
+	LOAD reg1 0 reg4 #load calculated volume
+	ADD reg4 reg5 reg5 #add to the sum
+	ADD|IMMB reg1 1 reg4 #increment second array index
+	IFNEQ|IMMB 31 SumLoop #loop over every second array item
+
+ADD|IMMB reg5 0 output #send the calculation to output
