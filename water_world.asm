5a47a365adbfff400b04f7ffe51c2fc391aae5e5
Samuel Jenks <mail@samueljenks.me>
2025-05-08 06:47:54 +1200

    draft: don't calc volumes if highest found

--- water_world.asm
+++ water_world.asm
@@ -1,26 +1,36 @@
 #reg0 = column index / loop1 (0 - 15)
 #reg1 = windback index (16 - 31)
 #reg2 = start point column
-#reg3 = windback column
+#reg3 = largest column (Loop)
+#reg3 = windback column (WindbackLoop)
 #reg4 = calculation
-#reg5 = previous column (algorithm stage)
-#reg5 = summed volume (final stage)
-#STACK = save point column index
+#reg5 = previous column (WindBackLoop)
+#reg5 = summed volume (SumLoop)
+#STACK0 = save point column index
+#STACK1 = save largest column
 
+IFEQ|IMMALL 0 0 4 #add breakpoint here
 label Loop
 	ADD|IMMB input 0 reg2 #read the current column height
 	STORE reg0 reg2 0 #store the column height in ram, first array
-	PUSH reg0 0 0 #save point column index
-
+	
 	IFNEQ|IMMB reg0 0 DontSkipColumn #skip the first column	
-	POP 0 0 reg0 #remove save point
 	ADD|IMMB reg2 0 reg5 #make current column the previous column
 	ADD|IMMB reg0 1 reg0 #increment current column index
 	IFEQ|IMMALL 0 0 Loop
 	label DontSkipColumn
+
+	IFLESSEU reg2 reg3 DontSaveLargest
+	ADD|IMMB reg2 0 reg3 #the new largest column
+	IFEQ|IMMALL 0 0 DontWindBack #skip checking this column since it has no backing
+	label DontSaveLargest
 	IFLESSEU reg2 reg5 DontWindBack #only sum when a positive gradient is detected
+	PUSH reg0 0 0 #save point column index
+	PUSH reg3 0 0 #save largest column
 	CALL 0 0 WindbackLoop
 	label DontWindBack
+
+	POP 0 0 reg3 #remove largest column
 	POP 0 0 reg0 #remove save point
 	ADD|IMMB reg2 0 reg5 #make current column the previous column
 	ADD|IMMB reg0 1 reg0 #increment column loop index
