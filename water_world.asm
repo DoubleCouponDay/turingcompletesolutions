0b4600f542dc094828c312d8aadf4206a10cac1d
Doublecouponday <mail@samueljenks.me>
2025-05-11 18:07:24 +1200

    draft: flood algorithm

--- water_world.asm
+++ water_world.asm
@@ -1,85 +1,82 @@
-#reg0 = column index / loop1 (0 - 15)
-#reg1 = windback index (16 - 31)
-#reg2 = start point column
-#reg3 = windback column (WindbackLoop)
-#reg4 = temp function pointer (WindbackLoop)
-#reg4 = temp previous previous gradient (WindBackLoop)
-#reg4 = calculation (WindbackLoop)
-#reg5 = end point column (WindBackLoop)
-#reg5 = summed volume (OutputLoop)
-#STACK0 = save point column index
+#reg0 = ColumnIndex (0 - 15)
+#reg1 = VolumeIndex (16 - 31)
+#reg2 = RowIndex (InputLoop)
+#reg3 = StartIndex (InputLoop)
+#reg4 = CurrentHeight (InputLoop, FindEnd, FindStart)
+#reg4 = CurrentVolume (OutputLoop)
+#reg5 = Calculation (InputLoop)
+#reg5 = EndIndex (InputLoop)
+#reg5 = Sum (OutputLoop)
 
 IFEQ|IMMALL 0 0 4 #add breakpoint here
+ADD|IMMALL -1 0 reg3 #save -1 to StartIndex
+ADD|IMMALL -1 0 reg4 #save -1 to EndIndex
+ADD|IMMALL 16 0 reg1 #default value for VolumeIndex
+ADD|IMMALL 1 0 reg2 #default value for row index
+
 label InputLoop
 	ADD|IMMB input 0 reg2 #read the current column height
 	STORE reg0 reg2 0 #store the column height in ram, first array
+	LOAD reg0 0 reg4 #load the current columns height
 	
-	IFNEQ|IMMB reg0 0 DontSkipColumn #skip the first column	
-	ADD|IMMB reg2 0 reg5 #make current column the previous column
-	ADD|IMMB reg0 1 reg0 #increment current column index
-	IFEQ|IMMALL 0 0 InputLoop
-	label DontSkipColumn
+	IFGREATU reg0 reg4 NotFillable #skip if (column, row) is not empty space
+	LOAD reg1 0 reg5 #get any saved volume for calculation
+	ADD reg4 reg5 reg5 #sum the volume plus height
+	ADD|IMMB reg5 1 reg5 #align with row index for comparison
+	IFNEQ reg2 reg5 NotFillable #skip if cell below (column, row) is not filled	
+	PUSH reg0 0 0 #backup ColumnIndex
+	CALL 0 0 FindEnd #call FindEnd for this cell
+	IFGREATS|IMMB reg5 -1 NotFillable #skip if EndIndex is -1
+	CALL 0 0 FindStart #call FindStart for this valid endindex
+	IFGREATS|IMMB reg3 -1 NotFillable #skip if StartIndex is -1
+	CALL 0 0 FloodRow #flood the row
+	POP 0 0 reg0 #reset ColumnIndex
+	ADD|IMMALL 16 0 reg0 #16 is the end of the loop. saves lines on incrementing row index
+	
+	label NotFillable #skip location1
+	IFGREATS|IMMB reg3 -1 Flooded #skip if StartIndex is not -1
+	POP 0 0 reg0 #bring back ColumnIndex backup
+	ADD|IMMB reg0 1 reg0 #increment ColumnIndex
+	ADD|IMMB reg0 16 reg1 #increment VolumeIndex
 
-	IFLESSEU reg2 reg5 DontWindBack #only sum when a positive gradient is detected
-	PUSH reg0 0 0 #save point column index
-	CALL 0 0 WindbackLoop #I have to use call and put the function pointer of the stack because I want to conditionally save. reduces stack overloading
-	POP 0 0 reg0 #remove save point
+	label Flooded #skip location2
+	ADD|IMMB reg3 0 reg3 #reset StartIndex
+	ADD|IMMB reg5 0 reg5 #reset EndIndex
+	IFNEQ|IMMB reg0 16 InputLoop #if ColumnIndex within bounds, continue looping
 
-	label DontWindBack
-	ADD|IMMB reg2 0 reg5 #make current column the previous column
-	ADD|IMMB reg0 1 reg0 #increment column loop index
-	IFNEQ|IMMB reg0 16 InputLoop #if column index within bounds, continue looping
+	ADD|IMMB reg2 1 reg2 #increment row index
+	ADD|IMMALL 0 0 #reset ColumnIndex
+	ADD|IMMALL 16 0 reg1 #reset VolumeIndex
+	IFNEQ|IMMB reg2 16 InputLoop #iterate InputLoop if row index not max
 
-ADD|IMMALL 0 0 reg5 #clear previous registers for second usage
-ADD|IMMALL 16 0 reg1 #reset second array index
+ADD|IMMALL 16 0 reg1 #reset reg1 for second usage
+ADD|IMMALL 0 0 reg5 #reset reg5 for second usage
 CALL 0 0 OutputLoop
 
-label WindbackLoop #start scanning backwards for the next local maximum
-	ADD|IMMB reg2 0 reg3 #set default previous value to starting column
-	label WindbackOne
-	SUB|IMMB reg0 1 reg0 #decrement
-	ADD|IMMB reg0 16 reg1 #set the windback index to second array, current column		
-	IFGREATES|IMMB reg0 0 NotSmallestIndex #boundary check
+label FindEnd
+	ADD|IMMB reg0 1 reg0 #increment ColumnIndex
+	LOAD reg0 0 reg4 #load the CurrentHeight
+	IFGREATU|IMMB reg2 reg4 NotSoil #continue looping if not soil
+	ADD|IMMB reg0 0 reg5 #save the EndIndex
 	RETURN 0 0 0
-	#Subtract the starting column by 1 to improve matches
-	#POP 0 0 reg4 #reset the column index to the starting point
-	#POP 0 0 reg0
-	#PUSH reg0 0 0
-	#PUSH reg4 0 0
-	#ADD|IMMALL 0 0 reg4
-	#ADD|IMMB reg0 16 reg1
-	#SUB|IMMB reg2 1 reg2 #decrease the starting column in case there could be a lower match
-	#IFGREATES|IMMB reg2 2 WindbackOne #Two is the minimum vessel column height
-	#RETURN 0 0 0
-	
-	label NotSmallestIndex
-	ADD|IMMB reg3 0 reg4
-	LOAD reg0 0 reg3 #load the previous column
-	IFLESSEU reg3 reg2 WindbackOne #stop winding back once a same height column is found
-	SUB reg3 reg4 reg4 #detect positive gradient on end. prevent endless slope level
-	IFLESSES|IMMB reg4 0 WindbackOne
-	
-	ADD|IMMB reg3 0 reg5 #use this as the end point column
-	POP 0 0 reg4
-	POP 0 0 reg0
-	PUSH reg0 0 0
-	PUSH reg4 0 0
-	ADD|IMMALL 0 0 reg4 #clear temp function pointer
-	LOAD reg0 0 reg2 #reset starting column size
-	
-	label WindBackTwo
-	SUB|IMMB reg0 1 reg0 #decrement
-	ADD|IMMB reg0 16 reg1
-	LOAD reg0 0 reg3
-	SUB reg2 reg3 reg4 #volume = difference of start point minus current windback column
-	IFLESSS|IMMB reg4 0 SkipSaving #skip bad volume
-	IFGREATS|IMMB reg4 0 NotStartingIndex #if its bigger than or equal to the starting column, stop winding back
+	label NotSoil
+	IFNEQ|IMMB reg0 16 FindEnd #continue looping if ColumnIndex is not 16
 	RETURN 0 0 0
-	label NotStartingIndex
-	IFEQ|IMMB reg0 0 SkipSaving #skip first column if it has no backing
-	STORE reg1 reg4 0 #store the volume in second array 
-	label SkipSaving
-	IFLESSU reg3 reg5 WindBackTwo #boundary check
+
+label FindStart
+	ADD|IMMB reg0 1 reg0 #decrement ColumnIndex
+	LOAD reg0 0 reg4 #load the CurrentHeight
+	IFGREATU|IMMB reg2 reg4 NotSoil #continue looping if not soil
+	ADD|IMMB reg0 0 reg3 #save the StartIndex
+	RETURN 0 0 0
+	label NotSoil
+	IFNEQ|IMMB reg0 0 FindStart #continue looping if ColumnIndex is not 0
+	RETURN 0 0 0
+
+label FloodRow
+	ADD|IMMB reg0 1 reg0 #increment ColumnIndex
+	ADD|IMMB reg0 16 reg1 #set VolumeIndex based on ColumnIndex
+	IFNEQ reg0 reg5 FloodRow #continue looping if ColumnIndex is not equal to EndIndex
 	RETURN 0 0 0
 
 label OutputLoop
